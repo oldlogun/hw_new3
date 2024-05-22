05e816d36ae5add23b4abd8fabc74f89c72f2c51
Vitaly <Vitaly@DESKTOP-0IE18EK>
2024-05-22 19:52:16 +0300

    Добавьте файлы проекта.

---
+++ hw_new3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_new3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int objectsInRow = 3;
+            int numberOfPictures = 52;
+            int numberOfRow = 52 / 3;
+            int picturesOutOfRange = 52 % 3;
+
+            Console.WriteLine("Количество рядов - " + (numberOfRow) + ". Картинки вне ряда - " + (picturesOutOfRange) + ".");
+        }
+    }
+}
